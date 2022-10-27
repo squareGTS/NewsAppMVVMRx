c21e774fa16f2441a6b9a0178e16bd92de6b67e3
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-27 20:32:57 +0300

    Add files via upload

---
+++ NewsAppMVVMRx/NewsAppMVVMRx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppMVVMRx
+//
+//  Created by Maxim Bekmetov on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
