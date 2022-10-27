77e734d3fc23e51adaaadaa3a278489c896c7a5b
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-27 21:28:43 +0300

    Implementing View Models

---
+++ NewsAppMVVMRx/NewsAppMVVMRx/View Models/ArticleViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  ArticleViewModel.swift
+//  NewsAppMVVMRx
+//
+//  Created by Maxim Bekmetov on 27.10.2022.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct ArticleListViewModel {
+    let articlesVM: [ArticleViewModel]
+}
+
+struct ArticleViewModel {
+    let article: Article
+
+    init(_ article: Article) {
+        self.article = article
+    }
+}
+
+extension ArticleViewModel {
+    var title: Observable<String> {
+        return Observable<String>.just(article.title)
+    }
+
+    var description: Observable<String> {
+        return Observable<String>.just(article.description ?? "")
+    }
+}
+
+extension ArticleListViewModel {
+    init(_ articles: [Article]) {
+        self.articlesVM = articles.compactMap(ArticleViewModel.init)
+    }
+}
+
+extension ArticleListViewModel {
+    func articleAt(_ index: Int) -> ArticleViewModel {
+        return self.articlesVM[index]
+    }
+}
