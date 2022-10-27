4194c55b861e135e5ec12549e0876b6e24bf3e93
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-27 21:19:55 +0300

    Fetching News Using RxSwift Extensions

--- NewsAppMVVMRx/NewsAppMVVMRx/Controllers/NewsTableViewController.swift
+++ NewsAppMVVMRx/NewsAppMVVMRx/Controllers/NewsTableViewController.swift
@@ -6,18 +6,32 @@
 //
 
 import UIKit
+import RxSwift
 
 class NewsTableViewController: UITableViewController {
+
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.prefersLargeTitles = true
+
+        populateNews()
     }
 
-    // MARK: - Table view data source
+    private func populateNews() {
+        let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=c3c25d770bd44a8eb00c0064f06067b5")!)
 
-   // override func numberOfSections(in tableView: UITableView) -> Int { 0 }
+        URLRequest.load(resource: resource).subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+
+
+
+    }
+
+    // override func numberOfSections(in tableView: UITableView) -> Int { 0 }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
 
